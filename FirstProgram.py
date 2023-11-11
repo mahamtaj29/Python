2df9e5fe39e5dd995d909bba38f2f50a1f5a8197
mahamtaj29 <136333552+mahamtaj29@users.noreply.github.com>
2023-11-11 12:37:08 +0200

    First Commit

---
+++ FirstProgram.py
@@ -0,0 +1,8 @@
+"""
+Course: COMP.CS.100, Programming 1: Introduction to Programming
+Name: Maham Taj
+Student ID: Fitech - ICT - Adult Learner
+Email Id: maham.taj@tuni.fi, maham.taj29@gmail.com
+"""
+
+print("Hello World!")
