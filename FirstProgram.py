0aa3c4a04512eb718c0551f4c0e3f74f0b2afa1b
mahamtaj29 <136333552+mahamtaj29@users.noreply.github.com>
2023-11-11 12:51:35 +0200

    Remove comments

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,8 +1,3 @@
-"""
-Course: COMP.CS.100, Programming 1: Introduction to Programming
-Name: Maham Taj
-Student ID: Fitech - ICT - Adult Learner
-Email Id: maham.taj@tuni.fi, maham.taj29@gmail.com
-"""
+
 
 print("Hello World!")
